dfe7d4e3d5b9b52ba4c00b1321eadf5012d8fa49
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-04-17 16:37:18 +0530

    Initial code for dashboard is started

--- fundoo/src/app/pages/dashboard/dashboard.ts
+++ fundoo/src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTooltipModule
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
